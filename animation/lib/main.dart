0190f428b8badc39a015022ee93b00ea00957c8f
Eduardo Henrique Kenji Shibukawa <EduardoShibuka@gmail.com>
2018-03-08 16:24:03 -0300

    navigation finalizado

--- animation/lib/main.dart
+++ animation/lib/main.dart
@@ -1,7 +1,20 @@
-
-
 import 'package:flutter/material.dart';
+import './loader.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MaterialApp(
+  home: new HomePage(),
+));
+
+class HomePage extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: new Center(
+        child: new Loader(),
+      ),
+    );
+  }
+}
 
 
